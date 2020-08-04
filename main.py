329967462490e27ab00d746b89fb237fc59f4c14
BayernMuller <cjjunk29@gmail.com>
2020-08-04 22:49:23 +0900

    Add: mouse event handlers

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ from widgets.LabelWidget import *
 class Main(QWidget):
     def __init__(self):
         super().__init__()
-        self.resize(1300,800)
+        self.resize(1500,800)
         self.setAttribute(Qt.WA_StyledBackground, True)
         self.setStyleSheet('Main { background-color: #0070C0 }')
         self.widgetBox = LabelWidget('white', 0, self)
         self.widgetBox.setGeometry(20,20,260,760)
         self.codeBox = LabelWidget('white', 0, self)
-        self.codeBox.setGeometry(300,120,980,660)
+        self.codeBox.setGeometry(300,120,1180,660)
+
+    def mousePressEvent(self, e):
+        print('clicked')
+
+    def mouseMoveEvent(self, e):
+        if (e.buttons() & ~Qt.NoButton):
+            print('moving')
+
+    def mouseReleaseEvent(self, e):
+        print('released')
         
 
 
